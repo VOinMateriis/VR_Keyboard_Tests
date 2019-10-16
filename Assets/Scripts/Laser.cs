8b1608f160eb51e4267b21fa9d30e8ea35f207e8
Verum VR Medical <verumvrmedical@gmail.com>
2019-10-16 13:59:17 -0500

    Laser tests

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -3,20 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 using UnityEngine.UI;
 
 public class Laser : MonoBehaviour
 {
     public GameObject keyboard;
     public SteamVR_Action_Boolean trigger;
-    GameObject button = null;
+    public GameObject hand;
+    GameObject currentUI = null;
 
     private bool hover;
+    private GameObject laser;
+    public float thickness = 0.002f;
+
+    private void Start()
+    {
+        transform.parent = hand.transform;
+        //transform.localPosition = Vector3.zero;
+
+        /*GameObject laser = transform.GetChild(0).gameObject;
+        laser.SetActive(true);*/
+
+
+        /*laser = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        laser.gameObject.name = "Laser";
+        laser.transform.parent = transform;
+        laser.transform.localScale = new Vector3(thickness, thickness, 100f);
+        laser.transform.localPosition = new Vector3(0f, 0f, 50f);
+        laser.transform.localRotation = Quaternion.identity;*/
+
+        LineRenderer laser = GetComponent<LineRenderer>();
+        //Transform sphere = transform.GetChild(0);
+
+        Vector3[] laserPoints = new Vector3[2];
+
+        laserPoints[0] = transform.parent.position;
+
+
+        //if (hit.collider != null)
+            //laserPoints[1] = hit.point;
+        //else
+            laserPoints[1] = transform.parent.forward * 3;
+
+
+        laser.SetPositions(laserPoints);
+        //sphere.position = laserPoints[1];
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = new Ray(transform.parent.position, transform.parent.forward);
 
         RaycastHit hit;
         PointerEventData pointer = new PointerEventData(EventSystem.current);
@@ -26,39 +64,75 @@ public class Laser : MonoBehaviour
         {
             if(hit.collider.CompareTag("Button") || hit.collider.CompareTag("InputField"))
             {
-                button = hit.collider.gameObject;
-                ExecuteEvents.Execute(button, pointer, ExecuteEvents.pointerEnterHandler);
+                currentUI = hit.collider.gameObject;
+
+                ExecuteEvents.Execute(currentUI, pointer, ExecuteEvents.pointerEnterHandler);
                 hover = true;
 
                 if (trigger.GetStateDown(SteamVR_Input_Sources.Any))
                 {
-                    if (button.CompareTag("InputField"))
+                    if (currentUI.CompareTag("InputField"))
                     {
-                        OpenKeyboard(button.GetComponent<InputField>());
+                        OpenKeyboard(currentUI.GetComponent<InputField>());
                     }
                     StartCoroutine(PressButton(pointer));
                 }
             }
-            
-                
         }
         else if (hover)
         {
-            ExecuteEvents.Execute(button, pointer, ExecuteEvents.pointerExitHandler);
+            ExecuteEvents.Execute(currentUI, pointer, ExecuteEvents.pointerExitHandler);
             hover = false;
         }
-            
 
-        Debug.DrawRay(transform.position, transform.forward * 1f);
+
+        //Debug.DrawRay(transform.position, transform.forward * 1f);
+
+        /*holder = new GameObject();
+        holder.transform.parent = this.transform;
+        holder.transform.localPosition = Vector3.zero;
+        holder.transform.localRotation = Quaternion.identity;*/
+
+
+        //BoxCollider collider = laser.GetComponent<BoxCollider>();
+
+        /*LineRenderer laser = GetComponent<LineRenderer>();
+        Transform sphere = transform.GetChild(0);
+
+        Vector3[] laserPoints = new Vector3[2];
+
+        laserPoints[0] = transform.parent.position;
+        
+
+        if(hit.collider != null)
+            laserPoints[1] = hit.point;
+        else
+            laserPoints[1] = transform.parent.forward * 3;
+
+
+        laser.SetPositions(laserPoints);
+        sphere.position = laserPoints[1];*/
+
+        LineRenderer laser = GetComponent<LineRenderer>();
+
+        Vector3[] laserPoints = new Vector3[2];
+
+        laserPoints[0] = transform.parent.position;
+        laserPoints[1] = transform.parent.forward * 3;
+
+
+        laser.SetPositions(laserPoints);
+
     }
 
 
     private IEnumerator PressButton(PointerEventData pointer)
     {
-        ExecuteEvents.Execute(button, pointer, ExecuteEvents.pointerClickHandler);
-        ExecuteEvents.Execute(button, pointer, ExecuteEvents.pointerDownHandler);
+        ExecuteEvents.Execute(currentUI, pointer, ExecuteEvents.pointerClickHandler);
+        ExecuteEvents.Execute(currentUI, pointer, ExecuteEvents.pointerDownHandler);
         yield return new WaitForSeconds(0.1f);
-        ExecuteEvents.Execute(button, pointer, ExecuteEvents.pointerUpHandler);
+        ExecuteEvents.Execute(currentUI, pointer, ExecuteEvents.pointerUpHandler);
+        ExecuteEvents.Execute(currentUI, pointer, ExecuteEvents.deselectHandler);   //Makes the UI element to get back its unhovered style
     }
 
 
---
+++ Assets/Scripts/ShowControllers.cs
@@ -0,0 +1,93 @@
+﻿//////////
+//  To hide the controllers and set the range of motion during game
+//  Script attached to: player
+//////////
+using System.Collections;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+public class ShowControllers : MonoBehaviour
+{
+    private Hand rightHand;
+    private Hand leftHand;
+    //Flags to indicate first iteration
+    private bool[] firsTime = new bool[2];
+    private Coroutine coroutine;
+
+
+    //================================================================================
+    void Start()
+    {
+        firsTime[0] = true;
+        firsTime[1] = true;
+    }
+
+
+    //================================================================================
+    void Update()
+    {
+        //Get hands
+        rightHand = Player.instance.rightHand;
+        leftHand = Player.instance.leftHand;
+
+
+        //CHECKS RIGHT HAND
+        //The first time the hands get active, hide the controllers (hand.skeleton = hand model)
+        if (rightHand.isPoseValid && rightHand.skeleton != null && firsTime[0])
+        {
+            StartCoroutine(SetupHand(rightHand));
+            firsTime[0] = false;
+        }
+        //If a hand is inactive, keeps checking for activation to hide the controllers
+        else if(rightHand.isPoseValid == false)
+        {
+            coroutine = StartCoroutine(CheckForControllerActive(rightHand));
+        }
+
+
+        //CHECKS LEFT HAND
+        if (leftHand.isPoseValid && leftHand.skeleton != null && firsTime[1])
+        {
+            StartCoroutine(SetupHand(leftHand));
+            firsTime[1] = false;
+        }
+        else if (leftHand.isPoseValid == false)
+        {
+            coroutine = StartCoroutine(CheckForControllerActive(leftHand));
+        }
+    }
+
+
+
+    //================================================================================
+    //If a hand is inactive (controller turned off), wait until it is activated and hide the controller
+    private IEnumerator CheckForControllerActive(Hand hand)
+    {
+        //Wait until the next frame to check if the controller has been turned on
+        yield return null;
+
+        //If controller has been turned on, hides it
+        if(hand.isPoseValid)
+        {
+            SetupHand(hand);
+        }
+    }
+
+
+
+    //================================================================================
+    //Hides the controllers and sets the range of motion
+    private IEnumerator SetupHand(Hand hand)
+    {
+        //Hide controllers
+        hand.HideController(false);
+       
+        //Waits until the hand model is loaded
+        while (hand.skeleton == null)
+            yield return null;
+
+        //Get a flat hand instead of one with the fingers around the controller
+        hand.skeleton.SetRangeOfMotion(EVRSkeletalMotionRange.WithController);
+    }
+}
