0c6b70aa327a67d91120415239e323bd8a5779e8
Verum VR Medical <verumvrmedical@gmail.com>
2019-10-18 14:02:54 -0500

    Tests on data persistence (Input field text)

---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
+public class SaveLoad : MonoBehaviour
+{
+    public InputField inputField;
+
+    private void Start()
+    {
+        Debug.Log("PATH: " + Application.persistentDataPath);
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/datosxd.banana");
+        
+        Name name = new Name();
+        name.name = inputField.text;
+
+        bf.Serialize(file, name);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/datosxd.banana"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/datosxd.banana", FileMode.Open);
+
+            Name name = new Name();
+            name = (Name)bf.Deserialize(file);
+            file.Close();
+
+            inputField.text = name.name;
+        }
+    }
+}
+
+[Serializable]
+class Name
+{
+    public string name;
+}
+
+
